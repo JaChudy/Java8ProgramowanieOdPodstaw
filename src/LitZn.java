13afeb19bbaf0347eeb390f15d7df79f2f046954
JaChudy <jakub.chudy@gmail.com>
2018-10-12 07:51:01 +0200

    Literały znakowe

---
+++ src/LitZn.java
@@ -0,0 +1,9 @@
+public class LitZn {
+    public static void main(String[] args){
+        System.out.print("\u03b1 \u03b2  \u03b4");
+        System.out.print('\\');
+        System.out.print('\'');
+        System.out.println("\nalpfa\nbeta\ngamma");
+        System.out.println("c:\\util\\back");
+    }
+}
